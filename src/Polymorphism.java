0d2daca49cbcec88e905265fe0dea4a22d6b7ebf
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-19 15:43:42 +0300

    Initial commit

---
+++ src/Polymorphism.java
@@ -0,0 +1,25 @@
+
+class  Bird{
+    public  void  sing(){
+        System.out.println("tweet tweet tweet");
+    }
+}
+
+
+
+class Robin extends Bird{
+    public  void  sing(){
+        System.out.println("tweet tweet tweet");
+    }
+}
+
+
+
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Robin b = new Robin();
+        b.sing();
+
+    }
+}
